17073caf39cc3a52da9e0890806504cfd4b25881
Usha Kiramai <ukganugula@gmail.com>
2018-03-12 08:25:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static('public'));
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.get('/',function(req,res){
+    res.sendFile( __dirname+ '/name.html');
+});
+
+app.post('/process', urlencodedParser, function (req, res) {
+	
+	var response = {
+firstname:req.body.fname,
+lastname:req.body.lname
+};
+console.log(response);
+res.send(response);
+})
+ app.listen(8081, function () {
+
+console.log("Example app listening at http://127.0.0.1:8081");
+});
